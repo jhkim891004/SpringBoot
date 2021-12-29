912ce1d7103e3441cb6123312bc75e8986ce2b20
yjhong <yjhong@cndfactory.com>
2021-12-29 21:51:30 +0900

    boardController 수정중

--- src/main/java/com/example/springboot/api/controller/board/BoardController.java
+++ src/main/java/com/example/springboot/api/controller/board/BoardController.java
@@ -1,17 +1,20 @@
 package com.example.springboot.api.controller.board;
 
+import java.util.List;
+
 import com.example.springboot.api.service.board.BoardService;
 import com.example.springboot.common.response.success.SuccessCode;
 import com.example.springboot.common.response.success.SuccessResponse;
 import com.example.springboot.model.dto.board.ResBoardSearchDTO;
-import io.swagger.annotations.Api;
-import io.swagger.v3.oas.annotations.Operation;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import lombok.RequiredArgsConstructor;
 
 @Api(tags = "Board")
 @RestController
@@ -26,6 +29,14 @@ public class BoardController {
 	}
 	
 	// TODO. 게시물 단건 조회
+	@Operation(summary = "단일 게시물 조회", description = "단일 게시물을 조회한다.")
+	@GetMapping("/api/v1/menu/board/{id}")
+	public SuccessResponse<ResBoardSearchDTO> getOneBoard(
+		@Parameter(description = "식별키", required = true, example = "1, 2, 3, ...")
+		@PathVariable("id") Long id) {
+			return new SuccessResponse<>(SuccessCode.OK, boardService.getOneBoard(id));
+	}
+
 	
 	// TODO. 게시물 추가
 	
